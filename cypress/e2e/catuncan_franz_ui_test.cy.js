456262c1639126921f018a948624a484355cb4d8
Franz Iljah Catuncan <fccatuncan@gmail.com>
2022-07-05 09:33:01 +0800

    Committed 5 Jul 2022

--- cypress/e2e/catuncan_franz_ui_test.cy.js
+++ cypress/e2e/catuncan_franz_ui_test.cy.js
@@ -3,19 +3,25 @@ const form_selector = 'form[name="myForm"]'
 const fullName_selector = 'input[id="fullName"]'
 const flexible_selector = 'input[id="flexible"]'
 const daysAvail_selector = 'input[id="daysAvail"]'
+const string100chars = 'Z5zJdWyc85mmF75thPeNcdUh2ItELbDEyIl7YFtNplpHMSR5X2o5FlQP3bp3DThGRl9aLIOvxbSDb0nqNHpykDQQljNFxzyg3tTr'
 
 describe('UI Tests', () => {
   it('Check Display of Page', () => {
     cy.visit('./activity.html')
     cy.get(form_selector).should('be.visible')
-    cy.get(fullName_selector).type('Franz Catuncan')
   });
+  it('Check Validation of Name Field', () => {
+    cy.get(fullName_selector).type(string100chars + '1').should('have.value',string100chars)
+
+  })
   it('Check Validation of Email Address Field', () => {
     const emailAddress_selector = 'input[id="emailAddress"]'
     const invalid_email = "this_is@inv"
 
+    cy.get(fullName_selector).type('Franz Catuncan')
     cy.get(emailAddress_selector).type(invalid_email)
     cy.get(form_selector).submit()
+    cy.wait(2000)
     cy.on('window:alert', (text) => {
       expect(text).to.contains('Please enter a valid email address!')
     });
